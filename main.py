7e7d7218a4280dcf7961aaed748570b9e87c4906
Dan Caron <dancaron@gmail.com>
2023-03-10 22:34:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ st.title('FENa Calculator')
 st.write('Enter the following values to calculate FENa:')
 
 # Create input fields for the necessary values
-urine_na = st.number_input(value=0.01, 'Urine Sodium Concentration (mmol/L)')
-serum_na = st.number_input(value=0.01, 'Serum Sodium Concentration (mmol/L)')
-urine_cr = st.number_input(value=0.01, 'Urine Creatinine Concentration (umol/L)')
-serum_cr = st.number_input(value=0.01, 'Serum Creatinine Concentration (umol/L)')
+urine_na = st.number_input('Urine Sodium Concentration (mmol/L)', value=0.01)
+serum_na = st.number_input('Serum Sodium Concentration (mmol/L)', value=0.01)
+urine_cr = st.number_input('Urine Creatinine Concentration (umol/L)', value=0.01)
+serum_cr = st.number_input('Serum Creatinine Concentration (umol/L)', value=0.01)
 
 # Call the calculate_fena function with the input values
 fena = calculate_fena(urine_na, serum_na, urine_cr, serum_cr)
